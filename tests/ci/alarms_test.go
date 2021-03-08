5b6cd04364899fee8ecac8f1f77a84b593e7f40c
kkouros <kristian.kouros@cern.ch>
2021-03-08 15:05:06 +0100

    retry the email test

--- tests/ci/alarms_test.go
+++ tests/ci/alarms_test.go
@@ -49,3 +49,13 @@ func TestCheckMinimumAlarm(t *testing.T) {
 
 }
 
+func TestSendNotification(t *testing.T) {
+	alias := "test.cern.ch"
+	recipient := "randomemail.cern.ch"
+	name := "minimum"
+	parameter := 1
+	err := alarms.SendNotification(alias, recipient, name, parameter)
+	if err != nil {
+		t.Errorf("Failed in TestSendNotification with Alias:%v\nRecipient:%v\nName:%v\nParameter:%v\nError:%v\n", alias, recipient, name, parameter, err)
+	}
+}
